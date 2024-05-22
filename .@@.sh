d02749d89c0e7f2135081d23ee51a09c16b2efed
lauxjar2 <lauxjar2@gmail.com>
2024-05-22 19:16:28 +0800

    20240522-整併安裝檔到.@@.sh

--- .@@.sh
+++ .@@.sh
@@ -1,5 +1,47 @@
 #!/bin/bash
+install() {
+    #先安裝docker ollama 和 open web ui
+    sudo yum remove docker \
+        docker-client \
+        docker-client-latest \
+        docker-common \
+        docker-latest \
+        docker-latest-logrotate \
+        docker-logrotate \
+        docker-engine
+    sudo yum install -y yum-utils
+    sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+    sudo yum install docker -y
+    sudo systemctl start docker
+    sudo systemctl enable docker
+    sudo yum install epel-release -y
+    sudo sudo amazon-linux-extras install epel -y
+    sudo yum install jq -y
+    sudo yum install tmux -y
+    sudo curl -fsSL https://ollama.com/install.sh | sh
+    echo "Please enter using GPU or not (y or n):"
+    read bGPUsupported
 
+    if [ "$bGPUsupported" == "y" ]; then
+        # GPU supported
+        sudo docker run -d --network=host --gpus=all -v ollama:/root/.ollama -v open-webui:/app/backend/data --name open-webui --restart always ghcr.io/open-webui/open-webui:ollama
+    else
+        # CPU supported
+        sudo docker run -d --network=host -v ollama:/root/.ollama -v open-webui:/app/backend/data --name open-webui --restart always ghcr.io/open-webui/open-webui:ollama
+    fi
+
+    sudo chmod 755 ~/.@@.sh
+
+    # add hot key to home folder's .bashrc
+    if [ $(cat ~/.bashrc | grep @@.sh | wc -l) -eq 0 ]; then
+        echo alias @=\'~/.@@.sh\' >>~/.bashrc
+    fi
+
+    # curl http://localhost:11434/api/create -d '{
+    #   "name": "aiagent111",
+    #   "modelfile": "FROM llama3 PARAMETER temperature 1 PARAMETER num_ctx 4096 PARAMETER num_thread 8"
+    # }'
+}
 #先安裝docker ollama 和 open web ui
 # sudo curl -fsSL https://ollama.com/install.sh | sh
 # sudo docker run -d --network=host --gpus=all -v ollama:/root/.ollama -v open-webui:/app/backend/data --name open-webui --restart always ghcr.io/open-webui/open-webui:ollama
@@ -102,10 +144,9 @@ if [ -z "$1" ]; then
     exit 1
 fi
 
+if [ "$1" == "install" ]; then
+    # Execute the ls -al command
+    install
+fi
 # Call the function with the first argument
 aiagent "$*"
-
-# add hot key to home folder's .bashrc
-if [ $(cat ~/.bashrc | grep @@.sh | wc -l) -eq 0 ]; then
-    echo alias @=\'~/.@@.sh\' >>~/.bashrc
-fi
