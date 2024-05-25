443dbf4b77a3c593da69465af39127da848641fb
lauxjar2 <lauxjar2@gmail.com>
2024-05-25 16:32:38 +0800

    20240525-刪除移除前docker指令。

--- .@@.sh
+++ .@@.sh
@@ -1,14 +1,6 @@
 #!/bin/bash
 install() {
     #先安裝docker ollama 和 open web ui
-    sudo yum remove docker \
-    docker-client \
-    docker-client-latest \
-    docker-common \
-    docker-latest \
-    docker-latest-logrotate \
-    docker-logrotate \
-    docker-engine
     sudo yum install -y yum-utils
     sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
     sudo yum install docker -y
